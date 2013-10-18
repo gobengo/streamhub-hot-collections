280e3696371263942a36e0d4a2dadd6a34386fa3
Benjamin Goering <bengoering@gmail.com>
2013-10-17 21:40:10 -0700

    Return value in example

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ define(function(require) {
     var HotCollectionsApp = require('streamhub-hot-collections');
 
     return function(el) {
-        new HotCollectionsApp({
+        return new HotCollectionsApp({
             el: el,
             network: 'thedailybeast.fyre.co'
         });
