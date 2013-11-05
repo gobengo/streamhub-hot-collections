ff21594fffcc649194be6c6199934936b0eecc96
Benjamin Goering <bengoering@gmail.com>
2013-11-05 11:29:29 -0800

    Make example.js wobble

--- example.js
+++ example.js
@@ -2,9 +2,22 @@ define(function(require) {
     var HotCollectionsApp = require('streamhub-hot-collections');
 
     return function(el) {
-        return new HotCollectionsApp({
+        var app = new HotCollectionsApp({
             el: el,
             network: 'thedailybeast.fyre.co'
-        }).start();
+        });
+
+        // Make wobbling Bars
+        var view = app._view;
+        var ogCreateMetricView = view._createMetricView;
+        view._createMetricView = function (metric) {
+            var barView = ogCreateMetricView.apply(this, arguments);
+            barView._wobble({
+                maxMultiplier: 0.1
+            });
+            return barView;
+        };
+
+        app.start();
     };
 });
